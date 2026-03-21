d5c1a3f806c12e33b8de1e200a1d9c57fb779e9b
Claire Kraft <c.kraft.9306@gmail.com>
2026-03-21 14:29:22 -0400

    reorganized quotes by human first names

--- quotes_bank.py
+++ quotes_bank.py
@@ -3,13 +3,50 @@
 # Format: { "Author Name": ["Quote one.", "Quote two.", ...], ... }
 
 QUOTES = {
+    "Barack Obama": [
+        "Better is good. That's the history of progress in this country-not perfect, better.",
+    ],
+
+    "Eric Schmidt": [
+        "If you read any management textbook, it says 'don't hire the divas' because they're "
+        "nothing but a pain in the ass. And by the way, they are. But the people who are the "
+        "divas-who believe-are the ones who will drive the culture and company to excellence.",
+    ],
+
     "Harvey MacKay": [
         "Time is free, but it's priceless. You can't own it, but you can use it. "
         "You can't keep it, but you can spend it. Once you've lost it you can never get it back.",
     ],
 
-    "Barack Obama": [
-        "Better is good. That's the history of progress in this country-not perfect, better.",
+    "Jack Ma": [
+        "If you want to be successful, learn from the other people's mistakes, don't learn from the successful stories.",
+
+        "Opportunities lie in the place where the complaints are.",
+
+        "Dream big, but start small. Act now.",
+
+        "A good boss is better than a good company.",
+
+        "I always find people smarter than I am. Then my job is to make sure smart people can work together. Stupid people can work together easily, smart people can't.",
+
+        "Never give up. Today is hard, tomorrow will be worse, but the day after tomorrow will be sunshine."
+        # "今天很残酷，明天更残酷, 后天很美好，但绝大多数人都死在明天晚上，看不见后天的太阳.", # Chinese chars not supported
+    ],
+
+    "Jensen Huang": [
+        "Strategy isn't words; strategy is action.",
+
+        "Never stop asking questions and seeking answers. Curiosity fuels progress.",
+
+        "Failure is not the end, it's an opportunity to learn and grow.",
+    ],
+
+    "My therapist": [
+        "Trust is earned in drops and lost in buckets.",
+    ],
+
+    "Samantha Power": [
+        "We decide, on issues large and small, whether we will be bystanders or upstanders.",
     ],
 
     "Steve Jobs": [
@@ -23,30 +60,12 @@ QUOTES = {
         "You can't connect the dots looking forward; you can only connect them looking "
         "backwards. So you have to trust that the dots will somehow connect in your future. "
         "You have to trust in something - your gut, destiny, life, karma, whatever. This "
-        "approach has never let me down, and it has made all the difference in my life.",  # ← comma here
+        "approach has never let me down, and it has made all the difference in my life.",
 
         "Details matter, it's worth waiting to get it right.",
 
         "My favorite things in life don't cost any money. It's really clear that the most "
         "precious resource we all have is time.",
-    ],
-
-    "Eric Schmidt": [
-        "If you read any management textbook, it says 'don't hire the divas' because they're "
-        "nothing but a pain in the ass. And by the way, they are. But the people who are the "
-        "divas-who believe-are the ones who will drive the culture and company to excellence.",
-    ],
-
-    "Jensen Huang": [
-        "Strategy isn't words; strategy is action.",
-
-        "Never stop asking questions and seeking answers. Curiosity fuels progress.",
-
-        "Failure is not the end, it's an opportunity to learn and grow.",
-    ],
-
-    "Samantha Power": [
-        "We decide, on issues large and small, whether we will be bystanders or upstanders.",
     ],
 
     "Susan Cain": [
@@ -55,7 +74,7 @@ QUOTES = {
         "Introversion-along with its cousins sensitivity, seriousness, and shyness-is now "
         "a second-class personality trait, somewhere between a disappointment and a pathology. "
         "Introverts living in the Extrovert Ideal are like women in a man's world, discounted "
-        "because of a trait that goes to the core of who they are.",  
+        "because of a trait that goes to the core of who they are.",
 
         "We have two ears and one mouth and we should use them proportionally.",
 
